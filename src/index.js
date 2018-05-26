05db8993cc94cbaf066982c50b6db42827b66e7b
Alex Kondratyuk <lynxtaa@gmail.com>
2018-05-26 22:11:48 +0300

    use spaces instead of tabs

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ const symbol = Symbol('Wrap customizer')
 const isErrorHandler = fn => fn.length > 3
 
 function wrap(fn) {
-	const wrapped = isErrorHandler(fn)
-		? (err, req, res, next) =>
-				fn(err, req, res, next)
-					.then(data => data !== undefined && wrapped[symbol](err, req, res, next)(data))
-					.catch(next)
-		: (req, res, next) =>
-				fn(req, res, next)
-					.then(data => data !== undefined && wrapped[symbol](req, res, next)(data))
-					.catch(next)
+  const wrapped = isErrorHandler(fn)
+    ? (err, req, res, next) =>
+        fn(err, req, res, next)
+          .then(data => data !== undefined && wrapped[symbol](err, req, res, next)(data))
+          .catch(next)
+    : (req, res, next) =>
+        fn(req, res, next)
+          .then(data => data !== undefined && wrapped[symbol](req, res, next)(data))
+          .catch(next)
 
-	wrapped[symbol] = (...args) => data =>
-		(isErrorHandler(fn) ? args[2] : args[1]).send(data)
+  wrapped[symbol] = (...args) => data =>
+    (isErrorHandler(fn) ? args[2] : args[1]).send(data)
 
-	return wrapped
+  return wrapped
 }
 
 wrap.custom = symbol
